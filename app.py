b8b495db5aa2a13bc40b42478de780fc54983385
bnihitha <bsknihitha@gmail.com>
2025-12-04 14:00:22 +0530

    added full files

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('form.html')
+
+@app.route('/submit', methods=['GET','POST'])
+def submit():
+    username = request.form['username']
+    return render_template('greeting.html', name=username)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug = True)
